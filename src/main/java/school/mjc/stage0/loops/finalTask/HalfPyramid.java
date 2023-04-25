49372291fc965d50e62af7210b4803e2af2163ae
Сункар Бауыржанулы <sunkar2710@gmail.com>
2023-04-25 17:22:50 +0600

    feat: implement stage0-module4-loops-taskFinal

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,7 +2,16 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
-
+        for (int y = 1; y <= cathetusLength; y++) {
+            for (int x = 1; x <= cathetusLength; x++) {
+                if (x > cathetusLength - y && x <= cathetusLength) {
+                    System.out.print("*");
+                    continue;
+                }
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,20 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-
+        int baseWidth = cathetusLength * 2;
+        for (int y = 1; y <= cathetusLength; y++) {
+            for (int x = baseWidth / 2; x >= 1; x--) {
+                if (x >= 1 && x <= y) {
+                    System.out.print(x);
+                    continue;
+                }
+                System.out.print(" ");
+            }
+            for (int x = 1 ; x < y; x++) {
+                System.out.print(x + 1);
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
